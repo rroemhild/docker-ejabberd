c50658b57d10f1baab2ccfd805fe465e0cdce5f0
Rafael Römhild <rafael@roemhild.de>
2015-12-19 16:08:04 +0100

    Cleanup run script

--- docker/run.sh
+++ docker/run.sh
@@ -21,7 +21,10 @@ fi
 
 
 run_scripts() {
-    local run_script_dir="${EJABBERD_HOME}/docker/${1}"
+    local run_script=$1
+    local run_script_dir="${EJABBERD_HOME}/docker/${run_script}"
+
+    log "Run ${run_script} scripts..."
     for script in ${run_script_dir}/*.sh ; do
         if [ -f ${script} -a -x ${script} ] ; then
             ${script}
@@ -30,48 +33,17 @@ run_scripts() {
 }
 
 
-pre_scripts() {
-    run_scripts "pre"
-}
-
-
-post_scripts() {
-    run_scripts "post"
-}
-
-stop_scripts() {
-    run_scripts "stop"
-}
-
-
-ctl() {
-    local action="$1"
-    ${EJABBERDCTL} ${action} >/dev/null
-}
-
-
 _trap() {
+    run_scripts "stop"
     log "Stopping ejabberd..."
-    stop_scripts
-    if ctl stop ; then
-        local cnt=0
-        sleep 1
-        while ctl status || test $? = 1 ; do
-            cnt=`expr $cnt + 1`
-            if [ $cnt -ge 60 ] ; then
-                break
-            fi
-            sleep 1
-        done
-    fi
+    $EJABBERDCTL stop
+    $EJABBERDCTL stopped
+    log "Ejabberd stopped."
 }
 
 
 # Catch signals and shutdown ejabberd
 trap _trap SIGTERM SIGINT
-
-log "Run pre scripts..."
-pre_scripts
 
 # print logfiles to stdout
 tail -F ${LOGDIR}/crash.log \
@@ -79,20 +51,20 @@ tail -F ${LOGDIR}/crash.log \
         ${LOGDIR}/erlang.log \
         ${LOGDIR}/ejabberd.log &
 
+# start ejabberd
+run_scripts "pre"
 log "Starting ejabberd..."
-ctl start
+$EJABBERDCTL start
+$EJABBERDCTL started
+log "Ejabberd started."
+run_scripts "post"
 
-sleep 2
-# ctl started
-
-log "Run post scripts..."
-post_scripts
-
-while ctl status || test $? = 0; do
+# keep running while ejabberd is up
+while ${EJABBERDCTL} status >/dev/null || test $? = 0; do
     sleep 1
 done
 
-log "Ejabberd stopped"
-stop_scripts
+log "Ejabberd stopped."
+
 
 exit 0
