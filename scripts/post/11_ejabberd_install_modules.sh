a4f1974de24d5a2f49c7331b52c81c8950d7deae
Andrew B <who@users.noreply.github.com>
2016-02-23 16:03:18 -0800

    refactoring module installation post script

--- scripts/post/11_ejabberd_install_modules.sh
+++ scripts/post/11_ejabberd_install_modules.sh
@@ -8,54 +8,61 @@ source "${EJABBERD_HOME}/scripts/lib/config.sh"
 source "${EJABBERD_HOME}/scripts/lib/base_functions.sh"
 source "${EJABBERD_HOME}/scripts/lib/functions.sh"
 
-copy_and_check_module() {
+install_module() {
     local module_name=$1
     local module_source_path=${EJABBERD_HOME}/module_source/${module_name}
     local module_install_folder=${EJABBERD_HOME}/.ejabberd-modules/sources/${module_name}
     
-    echo "Attempting to install module ${module_name}"
-    if file_exist ${module_source_path} ; then
-        cp -R ${module_source_path} ${module_install_folder}
-        
-        ${EJABBERDCTL} module_check ${module_name}
-        local retval=$?
+    echo "Analyzing module ${module_name} for installation"
+    # Make sure that the module exists in the source folder before attempting a copy
 
-        if [ ${retval} -ne 0 ]; then
-            echo "Module check failed for ${module_name}"
-        else 
-            echo "Module check succeeded for ${module_name}"
-        fi
-    else 
-        echo "Module ${module_name} not found in ${EJABBERD_HOME}/module_source. Skipping installation."
+    if [ ! -d ${module_source_path} ]; then
+        echo "Error: Module ${module_name} not found in ${EJABBERD_HOME}/module_source"
+        echo "Please use a shared volume to populate your module in ${EJABBERD_HOME}/module_source"
+        return 1;
     fi
+    # Check to see if the module is already installed
+    ${EJABBERDCTL} modules_installed
+    local install_count=$(${EJABBERDCTL} modules_installed | grep -ce "^${module_name}[[:space:]]")
+    if [ $install_count -gt 0 ]; then
+        echo "Error: Module already installed: ${module_name}"
+        return 1;
+    fi
+    # Copy the module into the shared folder
+    echo "Copying module to ejabberd folder ${module_install_folder}"
+    cp -R ${module_source_path} ${module_install_folder}
 
-    return $?
-}
+    # Run the ejabberdctl module_check on the module
+    echo "Running module_check on ${module_name}"
+    ${EJABBERDCTL} module_check ${module_name} >/dev/null
+    if [ $? -ne 0 ]; then
+        echo "Module check failed for ${module_name}"
+        return 1;
+    fi
+    echo "Module check succeeded for ${module_name}"
+    
 
-install_module() {
-    local module_name=$1
-
+    # Install the module
+    echo "Running module_install on ${module_name}"
     ${EJABBERDCTL} module_install ${module_name}
-    local retval=$?
-    if [ ${retval} -ne 0 ]; then
+    if [ $? -ne 0 ]; then
         echo "Module installation failed for ${module_name}"
-    else 
-        echo "Module installation succeeded for ${module_name}"
+        return 1;
     fi
+    echo "Module installation succeeded for ${module_name}"
 
-    return $?
+    return 0;
 }
 
-for module_name in ${EJABBERD_SOURCE_MODULES} ; do
-    copy_and_check_module ${module_name}
-    retval=$?
-    if [ ${retval} -eq 0 ]; then
+if [ -n "${EJABBERD_SOURCE_MODULES}" ]; then
+    for module_name in ${EJABBERD_SOURCE_MODULES} ; do
         install_module ${module_name}
-    fi
+    done
 
     if is_true ${EJABBERD_RESTART_AFTER_MODULE_INSTALL} ; then
-       ${EJABBERDCTL} restart
+        echo "Restarting ejabberd after successful module installation(s)"
+        ${EJABBERDCTL} restart
     fi
-done
+fi
 
 exit 0
